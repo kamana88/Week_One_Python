217f9401a49233a4d1594e4f8701c6f4659d0e61
kamana88 <kamanabrian88@gmail.com>
2024-02-24 15:58:59 +0300

    deployment

---
+++ user_input.py
@@ -0,0 +1,19 @@
+# name
+name = input("Enter your name: ")
+
+print("You entered:", name)
+
+# age
+age = input("Enter your age: ")
+
+print("You entered:", age)
+
+# location
+location = input("Enter your location: ")
+
+print("You entered:", location)
+
+# message
+message = "Hello " +name+ ", you are " +age+ " years old and live in "+location+ "."
+
+print(message)
